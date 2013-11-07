4c2d16de400621a2363405f347a173ed7d3b7194
Nicola Paolucci <npaolucci@atlassian.com>
2013-11-07 14:49:14 +0100

    Scripting everything to simplify initial setup

---
+++ build_and_deploy_stash_jira.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+
+sudo docker pull zaiste/postgresql
+sudo docker run -d -name postgres -p=5432:5432 zaiste/postgresql
+
+cd /vagrant
+./initialise_db.sh
+
+cd /vagrant/stash
+sudo docker build -t durdn/stash-2.8.2 .
+
+sudo docker run -d -name stash -link postgres:db -p 7990:7990 durdn/stash-2.8.2
+
+cd /vagrant/jira
+sudo docker build -t durdn/jira-6.1.1 .
+
+sudo docker run -d -name jira -link postgres:db -link stash:stash -p 8080:8080 durdn/jira-6.1.1
+
+"Containers running..."
+sudo docker ps
+
+"IP Addresses of containers:"
+paste <(sudo docker ps | tail -n +2 | awk {'printf "%s\t%s\n", $1, $2 '}) <(sudo docker ps  -q | xargs sudo docker inspect | tail -n +2 | grep IPAddress | awk '{ print $2 }' | tr -d ',"')
+
---
+++ initialise_db.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# From the trimmed down blog post, db creation is automated
+# ---------------------------------------------------------
+# We need to note down the IP address of the container so
+# that we can wire it up to Stash and JIRA later:
+# 
+#     sudo docker inspect postgres | grep IPAddress
+# 
+# Note down the IP Address. To test that PostgreSQL is running you can can
+# connect to it easily with (password: docker):
+# 
+#     psql -h <ip-address-noted> -d docker -U docker -W
+# 
+# (You may need to install the sql client with: `sudo apt-get install
+# postgresql-client`).
+# 
+# We'll need to setup databases for both Stash and JIRA, if you want to do it now
+# at the `psql` shell type:
+# 
+#     
+#     CREATE ROLE stashuser WITH LOGIN PASSWORD 'jellyfish' VALID UNTIL 'infinity';
+#     CREATE DATABASE stash WITH ENCODING='UTF8' OWNER=stashuser TEMPLATE=template0 CONNECTION LIMIT=-1;
+# 
+# The above creates `stash` database with `stashuser` user and password `jellyfish`. And for JIRA:
+# 
+#     CREATE ROLE jiradbuser WITH LOGIN PASSWORD 'jellyfish' VALID UNTIL 'infinity';
+#     CREATE DATABASE jiradb WITH ENCODING 'UNICODE' TEMPLATE=template0;
+# 
+# The above creates `jiradb` database with `jiradbuser` user and password `jellyfish`.
+# --------------------------------------------------------
+
+
+# Creates Stash and JIRA databases and users
+
+PSQL_IP=`sudo docker inspect postgres | grep IPAddress| cut -d '"' -f4`
+echo "
+CREATE ROLE stashuser WITH LOGIN PASSWORD 'jellyfish' VALID UNTIL 'infinity';
+CREATE DATABASE stash WITH ENCODING='UTF8' OWNER=stashuser TEMPLATE=template0 CONNECTION LIMIT=-1;
+CREATE ROLE jiradbuser WITH LOGIN PASSWORD 'jellyfish' VALID UNTIL 'infinity';
+CREATE DATABASE jiradb WITH ENCODING 'UNICODE' TEMPLATE=template0;" \
+| psql -h $PSQL_IP -d docker -U docker -w
+
