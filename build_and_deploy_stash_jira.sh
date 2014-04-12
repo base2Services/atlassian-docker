cd55baccb571e44b249ad22327576dead5f2427e
Brian Olsen <brian@maven-group.org>
2014-04-13 00:29:14 +0200

    Removed reference to absolute path /vagrant.

--- build_and_deploy_stash_jira.sh
+++ build_and_deploy_stash_jira.sh
@@ -1,16 +1,14 @@
 sudo docker pull zaiste/postgresql
 sudo docker run -d --name postgres -p=5432:5432 zaiste/postgresql
 
-cd /vagrant
+cd "$(dirname $0)"
 cat initialise_db.sh | sudo docker run --rm -i --link postgres:db zaiste/postgresql bash -
 
-cd /vagrant/stash
-sudo docker build -t durdn/stash-2.9.1 .
+sudo docker build -t durdn/stash-2.9.1 stash
 
 sudo docker run -d --name stash --link postgres:db -p 7990:7990 durdn/stash-2.9.1
 
-cd /vagrant/jira
-sudo docker build -t durdn/jira-6.1.1 .
+sudo docker build -t durdn/jira-6.1.1 jira
 
 sudo docker run -d --name jira --link postgres:db --link stash:stash -p 8080:8080 durdn/jira-6.1.1
 
