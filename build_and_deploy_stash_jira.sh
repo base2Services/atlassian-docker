3663194ecf513b3ffda8378d625ca6f4e5cb8480
Brian Olsen <brian@maven-group.org>
2014-04-13 01:23:27 +0200

    Detect need for sudo.

--- build_and_deploy_stash_jira.sh
+++ build_and_deploy_stash_jira.sh
@@ -1,19 +1,26 @@
-sudo docker pull zaiste/postgresql
-sudo docker run -d --name postgres -p=5432:5432 zaiste/postgresql
+if [ "$(docker run busybox echo 'test')" != "test" ]; then
+  SUDO=sudo
+  if [ "$($SUDO docker run busybox echo 'test')" != "test" ]; then
+    echo "Could not run docker"
+    exit 1
+  fi
+fi
+$SUDO docker pull zaiste/postgresql
+$SUDO docker run -d --name postgres -p=5432:5432 zaiste/postgresql
 
 cd "$(dirname $0)"
-cat initialise_db.sh | sudo docker run --rm -i --link postgres:db zaiste/postgresql bash -
+cat initialise_db.sh | $SUDO docker run --rm -i --link postgres:db zaiste/postgresql bash -
 
-sudo docker build -t durdn/stash-2.9.1 stash
+$SUDO docker build -t durdn/stash-2.9.1 stash
 
-sudo docker run -d --name stash --link postgres:db -p 7990:7990 durdn/stash-2.9.1
+$SUDO docker run -d --name stash --link postgres:db -p 7990:7990 durdn/stash-2.9.1
 
-sudo docker build -t durdn/jira-6.1.1 jira
+$SUDO docker build -t durdn/jira-6.1.1 jira
 
-sudo docker run -d --name jira --link postgres:db --link stash:stash -p 8080:8080 durdn/jira-6.1.1
+$SUDO docker run -d --name jira --link postgres:db --link stash:stash -p 8080:8080 durdn/jira-6.1.1
 
 echo "Containers running..."
-sudo docker ps
+$SUDO docker ps
 
 echo "IP Addresses of containers:"
-sudo docker inspect -f '{{ .Config.Hostname }} {{ .Config.Image }} {{ .NetworkSettings.IPAddress }}' postgres stash jira
+$SUDO docker inspect -f '{{ .Config.Hostname }} {{ .Config.Image }} {{ .NetworkSettings.IPAddress }}' postgres stash jira
