f3738767ea666de6fe45aefb044a0a38d7776fd2
Brian Olsen <brian@maven-group.org>
2014-04-24 08:25:14 +0200

    Made /opt/stash-home into a volume.

--- stash/launch.bash
+++ stash/launch.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o errexit
 
+sudo own-volume
+
 if [ -z "$STASH_HOME" ]; then
   export STASH_HOME=/opt/stash-home
 fi
---
+++ stash/own-volume.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec chown stash:stash /opt/stash-home
